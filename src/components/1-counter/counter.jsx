24e9a942971a0a89b3aa4101688cafd4fc97eacc
Your Name <you@example.com>
2023-02-08 15:44:46 +0300

    first commit

---
+++ src/components/1-counter/counter.jsx
@@ -0,0 +1,28 @@
+import './index.scss';
+import React, { useState } from 'react';
+
+function Counter() {
+    const [number, setNumber] = useState=(0);
+    return (
+        <div classNmae={'body_counter'}>
+            <div className='counter'>
+                <div>
+                    <h2>Счетчик</h2>
+                    <h1>{number}</h1>
+                    <button className="minus" onClick={() => setNumner(number - 1)}> 
+                        - Минус
+                    </button>
+                    <button className="plus" onClick={() => setNumber(number + 1)}>
+                        Плюс +
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Counter;
+
+
+
+
---
+++ src/components/nav.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export const Nav = () => {
+    const navBar = ['counter', 'modal', 'quiz', 'users', 'converter', 'photos'];
+
+    return (
+        <div>
+            <div className={'nav'}>
+                <span className={'navText'}>
+                    <Link to={'/'}>Главная</Link>
+                </span>
+
+                {navBar.map((el) => {
+                    <span className={'navText'}>
+                        <Link to={el}>{el}</Link>
+                    </span>
+                })}
+            </div>
+        </div>
+    )
+}
